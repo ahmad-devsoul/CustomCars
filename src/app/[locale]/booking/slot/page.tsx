8f3240fa255a1a020bf50d9f40dc12fb98d0bf7e
ahmad <ahmad.raza.devsoul@gmail.com>
2025-02-03 15:46:56 +0500

    order hide on mobile screen

--- src/app/[locale]/booking/slot/page.tsx
+++ src/app/[locale]/booking/slot/page.tsx
@@ -11,19 +11,24 @@ export default function Slots() {
     <div className="overflow-hidden md:h-screen bg-gray-50">
       <div className="rounded-xl shadow-sm">
         <div className="flex flex-col md:flex-row justify-between w-full">
-          <div dir={locale === 'ar' ? 'rtl' : 'ltr'} className="w-full px-5 md:px-10 mt-5 lg:mt-8">
+          <div
+            dir={locale === "ar" ? "rtl" : "ltr"}
+            className="w-full px-5 md:px-10 mt-5 lg:mt-8"
+          >
             <h1 className="px-1 py-3 lg:py-5 font-semibold text-2xl text-black">
-              {t('Select Date and Time')}
+              {t("Select Date and Time")}
             </h1>
             <div className="flex flex-col lg:flex-row w-full">
               <Calendar />
               <TimeSlots />
             </div>
           </div>
-          <div className="px-5 md:px-0 w-full md:w-[45%] mb-20 md:mb-0"><OrderSummary /></div>
+          <div className="px-5 md:px-0 w-full md:w-[45%] mb-20 md:mb-0 hidden md:block">
+            <OrderSummary />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
